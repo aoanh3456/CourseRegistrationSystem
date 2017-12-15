cc89368b7f759ee39fea523e3b34735e2590aabf
Nhien <tramhanhien97@gmail.com>
2017-12-15 23:22:14 +0700

    nhien

--- PHP_File/Manage.php
+++ PHP_File/Manage.php
@@ -4,12 +4,23 @@
 <html xmlns="http://www.w3.org/1999/xhtml" lang="en">
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-		<title>EnrollCourse</title>
+		<title>Manage</title>
 		<meta name="author" content="Rummy" />
 		<!-- Date: 2017-12-14 -->
 		<link rel="stylesheet" type="text/css" href="../CSS_File/menuStudentCSS.css">
+
 	</head>
-	<body>
+     <body>
+		<table border="1" style="width:50%">
+			<tr>
+				<th>StudentID</th>
+				<th>StudentName</th>
+				<th>CourseName</th>
+				<th>Date</th>
+				<th>Status</th>
+			</tr>
+		</table>	
+
 		<form name="form" method="post" action="">
 		<input type="hidden" name="idStudent" value="" >
 			<?php
@@ -17,10 +28,7 @@
 			?>
 			<div id="main">
 				<table>
-					<tr>
-						<td>Semester:</td>
-						<td></td>
-					</tr>
+				
 				</table>
 			</div>
 		</form>
