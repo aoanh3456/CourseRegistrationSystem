ca30dbd50666455458826d7880d1cd4b6dfe91ba
Nhien <tramhanhien97@gmail.com>
2017-12-15 22:22:24 +0700

    nhien

---
+++ PHP_File/Profile.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN"
+"http://www.w3.org/TR/html4/strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<title>Profile</title>
+		<meta name="author" content="Rummy" />
+		<!-- Date: 2017-12-14 -->
+		<link rel="stylesheet" type="text/css" href="../CSS_File/menuStudentCSS.css">
+	</head>
+	<body>
+		<body>
+		<form name="Profile" method="post" action="">
+			<div class="main">
+				Profile
+				<table border="0">
+				<tr>
+					<td>Student ID:</td>
+					<td><input type="text" name="Stuid" ></td>
+				</tr>
+				<tr>
+					<td>FirstName:</td>
+					<td><input type="text" name="firstname">  </td>
+				</tr>
+				<tr>
+					<td>LastName:</td>
+					<td><input type="text" name="lastname"> </td>
+				</tr>
+				<tr>
+					<td>Intake:</td>
+					<td><input type="text" name="intake"> </td>
+				</tr>
+				<tr>
+					<td>Gender:</td>
+					<td><input type="text" name="gender"></td>
+				</tr>
+				<tr>
+					<td>Telephone:</td>
+					<td><input type="tel" name="usrtel"> </td>
+				</tr>
+				<tr>
+					<td>Major</td>
+					<td><input type="text" name="major"></td>
+				</tr>
+				<td><input type="button" value ="Change Password"><br></td>
+				</table>
+			</div>
+		<form name="Profile" method="post" action="">
+			
+	</body>
+</html>
