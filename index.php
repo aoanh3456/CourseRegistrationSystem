f92d8620916ebca29ce5169fe0dbcc452c216d0f
nhienhcm <34519499+nhienhcm@users.noreply.github.com>
2017-12-22 09:25:44 +0700

    nhien

--- index.php
+++ index.php
@@ -12,12 +12,11 @@ session_start();
 		<meta name="author" content="NhienTran" />
 		<!-- Date: 2017-12-14 -->
 		<style>
-		table {
-    border: 300px ;
-    padding: 100px;
-   
+		table{
+  
+    width: 100%;
+	border: 0px solid;
 }
-
 
 * {
     box-sizing: border-box;
@@ -31,18 +30,21 @@ body {
 .column {
     float: left;
     padding: 0px;
-    height: 300px;
+    height: 638px;
 }
 
 .left {
   width: 60%;
-
+   
 }
 
 .right {
-  width: 40%;
+  width:40%;
  
-  
+  background-color: #1E90FF;
+  border: 0px solid black;
+  }
+
 }
 
 
@@ -55,57 +57,68 @@ body {
 			<div class="row">
   <div class="column left"  >
   
-    <img src="Img/Courses2.jpg" style="width:900px;height:600px;">
+    	<img src="Img/Courses2.png" style="width:877px;height:637px;">
   
   </div>
+ 
   <div class="column right" align="center">
-	
-  
-    
-    	<td colspan="2">
-    		<img src="Img/Courses5.png" style="width:300px;height:300px;"  >
-    		
-    	</td>
-    
-                      
-							<table border="0" >	
-								<tr>
-                            		<td>Username:</td>
-									<td><input type="text" name="username"  value="<?php if (isset($_SESSION["username"])){echo (string)$_SESSION["username"];}?>"></td>
-								</tr>
-								<tr>
-									<td>Password:</td>
-									<td><input type="password" name="password"></td>
-								</tr>
-								<?php 
-									
-									$valid="1";
-									if (isset($_SESSION["valid"])){
-										   $valid=(string)$_SESSION["valid"];
-									}
-									if($valid=="0"){
-								?>
-								<tr>
-									<td colspan="2" align="center">Invalid account!</td>
-								</tr>
-								<?php  }
-									if (isset($_SESSION["valid"])){
-										unset($_SESSION["valid"]);
-									}
-								?>
-								<div>
-								<tr>
-									<td align="center" colspan="2"><input type="submit" name="signinbotton" value ="Login"></td>
-								</tr>
-					</div>
-								</table>
-								
-  </div>
-</div>
-			
-			
-				
-		
+         <td>
+             <img src="Img/Courses5.png" style="width:300px;height:300px;">	
+         </td>
+         <div >
+                                 <table border="1"   >	
+                                 <tr>
+                                    <td height="50">
+                                     <input type="text" name="username" placeholder="Username" style="width:300px"  value="<?php if (isset($_SESSION["username"])){echo (string)$_SESSION["username"];}?>">
+                                     </td>
+                                 </tr>
+                                 
+                                 <tr>
+                                    
+                                     <td height="10
+                                     
+                                     
+                                     0" width="50">
+                                     	<input type="password" name="password" placeholder="Password" >
+                                     	</td>
+                                 </tr>
+                                 <?php 
+                                                                                  $valid="1";
+                                     if (isset($_SESSION["valid"])){
+                                            $valid=(string)$_SESSION["valid"];
+                                     }
+                                     if($valid=="0"){
+                                 ?>
+                                 <tr>
+                                     <td colspan="2" align="left">Invalid account!</td>
+                                 </tr>
+                                 <?php  }
+                                     if (isset($_SESSION["valid"])){
+                                         unset($_SESSION["valid"]);
+                                     }
+                                 ?>
+                                 <tr>
+                                 	
+                                     <td align="left"  >
+                                     	<input type="submit" name="signinbotton" value ="Login" style="background-color: #FFFFF; 
+    									border: 1 solid px;
+    									color: black;
+   										padding: 10px;
+    									width: 20%;
+    									text-align: center;
+    									text-decoration: none;
+    									font-size: 16px;"  >
+                                     	</td>
+                                     
+                                 </tr>
+                                 <h2></h2>
+                                 <tr>
+                                 </tr>
+                                 </table>
+                                 </div>
+                                 </div>
+ 
+		</div>
 		</form>
 	</body>
 </html>
